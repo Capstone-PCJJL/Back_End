6aa439caa7acdc40947a0b660cfe931881fa1af4
Jojo Ngai <jojo.ngai2003@gmail.com>
2025-05-29 15:44:27 -0400

    sql_connection

---
+++ sqlconnection.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+SQL_HOST = os.getenv("SQL_HOST")
+SQL_PORT = os.getenv("SQL_PORT") 
+SQL_USER = os.getenv("SQL_USER")
+SQL_PASS = os.getenv("SQL_PASS")
+SQL_DB   = os.getenv("SQL_DB")
+
+DATABASE_URL = f"mysql+pymysql://{SQL_USER}:{SQL_PASS}@{SQL_HOST}:{SQL_PORT}/{SQL_DB}"
+
+engine = create_engine(DATABASE_URL)
+
+# Test the connection
+with engine.connect() as conn:
+    result = conn.execute("SELECT VERSION();")
+    print("Database version:", result.fetchone()[0])
