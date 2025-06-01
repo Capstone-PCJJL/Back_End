7bdf4248bdb63a5c62a1f3abdd7813e732ce5d78
Jojo Ngai <jojo.ngai2003@gmail.com>
2025-05-31 22:01:38 -0400

    changed importMovieLens directory

--- sqlconnection.py
+++ sqlconnection.py
@@ -2,17 +2,20 @@ from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
 
-load_dotenv()
+def create_db_engine():
+    load_dotenv()
 
-SQL_HOST = os.getenv("SQL_HOST")
-SQL_PORT = os.getenv("SQL_PORT") 
-SQL_USER = os.getenv("SQL_USER")
-SQL_PASS = os.getenv("SQL_PASS")
-SQL_DB   = os.getenv("SQL_DB")
+    SQL_HOST = os.getenv("SQL_HOST")
+    SQL_PORT = os.getenv("SQL_PORT")
+    SQL_USER = os.getenv("SQL_USER")
+    SQL_PASS = os.getenv("SQL_PASS")
+    SQL_DB   = os.getenv("SQL_DB")
 
-DATABASE_URL = f"mysql+pymysql://{SQL_USER}:{SQL_PASS}@{SQL_HOST}:{SQL_PORT}/{SQL_DB}"
+    DATABASE_URL = f"mysql+pymysql://{SQL_USER}:{SQL_PASS}@{SQL_HOST}:{SQL_PORT}/{SQL_DB}"
 
-engine = create_engine(DATABASE_URL)
+    engine = create_engine(DATABASE_URL)
 
-with engine.connect() as conn:
-    print('successful connection')
+    with engine.connect() as conn:
+        print("✅ Successful connection")
+
+    return engine
